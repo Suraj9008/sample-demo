3b5e380bf16bc56a40202a5523a34971430ec2e3
suri@9008 <surajnarulesp108@gmail.com>
2021-07-26 19:51:49 +0530

    1st commit

---
+++ src/app/_services/socialmedia.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class SocialmediaService {
+
+  constructor( private http:HttpClient ) { }
+
+  accesstoken(){
+      // this.http.
+  }
+  
+}
--- src/app/views/contact/import/import.component.ts
+++ src/app/views/contact/import/import.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-import',
   templateUrl: './import.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    
+  }
 
   ngOnInit(): void {
+    console.log(this.router.url)
   }
 
 }
