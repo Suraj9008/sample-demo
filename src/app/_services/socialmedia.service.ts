70c4540d98a8de4da2c72f423fe1d6a7dc68841b
suri@9008 <surajnarulesp108@gmail.com>
2021-07-27 13:15:00 +0530

    best commit

--- src/app/_services/socialmedia.service.ts
+++ src/app/_services/socialmedia.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient,HttpHeaders } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class SocialmediaService {
   constructor(private http: HttpClient) { }
-  url = 'https://angular-crm-backend.herokuapp.com/';
 
   accesstoken() {
-    return this.http.get('https://angular-crm-backend.herokuapp.com/linkedin/auth/linkedin');
+    let httpheader = new HttpHeaders().set("Access-Control-Allow-Origin", "*")
+
+    return this.http.get('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86bb74gj1gubjr&redirect_uri=https://sample-website-linkedin.herokuapp.com&state=foobar&scope=r_liteprofile%20r_emailaddress%20w_member_social',{headers:httpheader});
   }
 }
